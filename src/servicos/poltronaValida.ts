3f0bb23152a36f8cfd2e4fe23ed8eeaf1404ac37
Gabriel Couto <gabriel.couto2704@gmail.com>
2026-09-13 19:35:59 -0300

    feat: resolve question 2

--- src/servicos/poltronaValida.ts
+++ src/servicos/poltronaValida.ts
@@ -19,4 +19,16 @@ export function fileirasDaSala(capacidade: number): string {
   return FILEIRAS.substring(0, capacidade / 10);
 }
 
-// TODO: export function poltronaValida(...) { ... }
+export function poltronaValida(fileira: string, numero: number): boolean {
+  const letra: string = fileira.trim().toUpperCase().charAt(0); // pega a letra da fileira (indice 0), sem espacos e maiuscula
+  const fileiras: string = fileirasDaSala(numero); // pega as fileiras (usa a funcao pronta fileirasDaSala)
+  const numeroPoltronas: number = Number(fileira.trim().substring(1));
+
+  return (
+    letra.length === 1 &&
+    fileiras.includes(letra) &&
+    Number.isInteger(numeroPoltronas) &&
+    numeroPoltronas >= 1 &&
+    numeroPoltronas <= 10 // caso TUDO for 'true', devolve 'true', se nao devolve 'false'
+  );
+}
